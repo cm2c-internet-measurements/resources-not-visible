fc1021f78ef62aab360e74b68b0ca2a192b3e09b
Carlos Martinez <carlos@xt6.us>
2019-11-16 14:00:17 +0800

    jupyter report and other improvements

--- s0_get_netdatadb.sh
+++ s0_get_netdatadb.sh
@@ -3,20 +3,30 @@
 # d='20191006'
 
 d=$1
+nf="var/netdata-$d.db.gz"
 
 if [ -z "$d" ]
 then
-    echo Invoke as: ./s0_get_netdatadb.sh YYYYMMDD
+    echo ERROR - Invoke as: ./s0_get_netdatadb.sh YYYYMMDD
     exit 1
 fi
 
-echo Getting netdata for date $d
+if [ -f "$nf" ]
+then
+    echo WARNING - File $nf already exists. Skipping download.
+    echo WARNING - Please remove existing file to force re-downloading.
+    exit 0
+else
+    echo Getting netdata for date $d
 
-wget -c --output-document=var/netdata-$d.db.gz \
-    http://trantor.labs.lacnic.net/carlos/netdata/netdata-$d.db.gz
+    wget -c --output-document="$nf" \
+        http://trantor.labs.lacnic.net/carlos/netdata/netdata-$d.db.gz
 
-cd var
+    cd var
+    gunzip netdata-$d.db.gz
+fi 
 
-gunzip netdata-$d.db.gz
-
+# linking to latest
 ln -sf netdata-$d.db netdata-latest.db 
+
+exit 0
--- start_jupyter.sh
+++ start_jupyter.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
 # Assumes docker installed. Will pull image if needed.
-docker run -p 8888:8888 -v "$PWD"/reports:/home/jovyan/var jupyter/scipy-notebook:17aba6048f44 &
+docker run -p 8888:8888 \
+   -e JUPYTER_ENABLE_LAB=yes \
+   -v "$PWD"/reports:/home/jovyan/reports \
+   -v "$PWD"/var:/home/jovyan/var \
+   jupyter/scipy-notebook:17aba6048f44 \
+   start-notebook.sh --NotebookApp.token='' & 
 
 open http://127.0.0.1:8888/
